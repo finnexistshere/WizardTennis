97a4f98f0de949f97e673f7e81cb7e64038be3e0
Och0 <jasperpringle@gmail.com>
2025-10-23 16:53:19 +1100

    anim changes

--- Wizard Tennis - V3/Assets/Scripts/Player Animator.cs
+++ Wizard Tennis - V3/Assets/Scripts/Player Animator.cs
@@ -5,8 +5,7 @@ public class TwoHandIKController : MonoBehaviour
 {
     [Header("Controller & Targets")]
     [SerializeField] private Transform twoHandController; // Main controller
-    [SerializeField] private Transform leftHandTarget;
-    [SerializeField] private Transform rightHandTarget;
+    [SerializeField] private Transform playerPos;
 
     [Header("Extras")]
     [SerializeField] private Transform ball; // Ball
@@ -14,8 +13,12 @@ public class TwoHandIKController : MonoBehaviour
 
     private void LateUpdate()
     {
-        // Move controller to ball
-        twoHandController.position = ball.position;
+        Vector3 targetPosition = ball.position;
+        targetPosition.y = twoHandController.position.y + 0.5f;
+        Vector3 pos = Vector3.MoveTowards(playerPos.position, targetPosition, 0.3f);
+        //pos.y = twoHandController.position.y + 0.5f;
+
+        twoHandController.position = pos;
 
         rig.Build(); // Apply transformations to rig
     }
