b5c2962abe2437f5a364f005355d1e59059e66a0
finnexistshere <kimia11@icloud.com>
2026-01-31 17:02:52 +1100

    Multiplayer Spell Customisation Test 3 (attempt2)

--- Wizard Tennis - V4 - 09-10-2025/Assets/Scripts/Networking/NetworkedPlayerCustomisation.cs
+++ Wizard Tennis - V4 - 09-10-2025/Assets/Scripts/Networking/NetworkedPlayerCustomisation.cs
@@ -98,6 +98,31 @@ public class NetworkedPlayerCustomisation : NetworkBehaviour
 
         if (debugLog)
             Debug.Log($"[NetworkedCustomisation] Spawned for {(IsOwner ? "Owner" : "Remote")} - ClientId: {OwnerClientId}");
+
+        NetworkedGameManager gameManager = GameObject.Find("Game Manager").GetComponent<NetworkedGameManager>();
+        if (OptionsManager.Instance != null)
+        {
+            if (OptionsManager.Instance.FireballBool) playerPrefabs.Add(gameManager.pickupPrefabs[0]);
+            if (OptionsManager.Instance.IceBool) playerPrefabs.Add(gameManager.pickupPrefabs[1]);
+            if (OptionsManager.Instance.LightningBool) playerPrefabs.Add(gameManager.pickupPrefabs[2]);
+            if (OptionsManager.Instance.ShadowBool) playerPrefabs.Add(gameManager.pickupPrefabs[3]);
+            if (OptionsManager.Instance.GreenBool) playerPrefabs.Add(gameManager.pickupPrefabs[4]);
+            if (OptionsManager.Instance.StoneBool) playerPrefabs.Add(gameManager.pickupPrefabs[5]);
+            if (OptionsManager.Instance.ChronosBool) playerPrefabs.Add(gameManager.pickupPrefabs[6]);
+            if (OptionsManager.Instance.GeminiBool) playerPrefabs.Add(gameManager.pickupPrefabs[7]);
+            if (OptionsManager.Instance.PiscesBool) playerPrefabs.Add(gameManager.pickupPrefabs[8]);
+            if (OptionsManager.Instance.JollyBool) playerPrefabs.Add(gameManager.pickupPrefabs[9]);
+            if (OptionsManager.Instance.BlinkBool) playerPrefabs.Add(gameManager.pickupPrefabs[10]);
+            if (OptionsManager.Instance.WarpBool) playerPrefabs.Add(gameManager.pickupPrefabs[11]);
+            if (OptionsManager.Instance.TetherBool) playerPrefabs.Add(gameManager.pickupPrefabs[12]);
+            if (OptionsManager.Instance.MudBool) playerPrefabs.Add(gameManager.pickupPrefabs[13]);
+            if (OptionsManager.Instance.GambitBool) playerPrefabs.Add(gameManager.pickupPrefabs[14]);
+            if (OptionsManager.Instance.GorbinoBool) playerPrefabs.Add(gameManager.pickupPrefabs[15]);
+        }
+        else if (OptionsManager.Instance == null)
+        {
+            playerPrefabs = gameManager.pickupPrefabs;
+        }
     }
 
     public override void OnNetworkDespawn()
@@ -412,31 +437,31 @@ public class NetworkedPlayerCustomisation : NetworkBehaviour
         ApplyCustomisation();
     }
 
-    private void Awake()
-    {
-        GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        if (OptionsManager.Instance != null)
-        {
-            if (OptionsManager.Instance.FireballBool) playerPrefabs.Add(gameManager.pickupPrefabs[0]);
-            if (OptionsManager.Instance.IceBool) playerPrefabs.Add(gameManager.pickupPrefabs[1]);
-            if (OptionsManager.Instance.LightningBool) playerPrefabs.Add(gameManager.pickupPrefabs[2]);
-            if (OptionsManager.Instance.ShadowBool) playerPrefabs.Add(gameManager.pickupPrefabs[3]);
-            if (OptionsManager.Instance.GreenBool) playerPrefabs.Add(gameManager.pickupPrefabs[4]);
-            if (OptionsManager.Instance.StoneBool) playerPrefabs.Add(gameManager.pickupPrefabs[5]);
-            if (OptionsManager.Instance.ChronosBool) playerPrefabs.Add(gameManager.pickupPrefabs[6]);
-            if (OptionsManager.Instance.GeminiBool) playerPrefabs.Add(gameManager.pickupPrefabs[7]);
-            if (OptionsManager.Instance.PiscesBool) playerPrefabs.Add(gameManager.pickupPrefabs[8]);
-            if (OptionsManager.Instance.JollyBool) playerPrefabs.Add(gameManager.pickupPrefabs[9]);
-            if (OptionsManager.Instance.BlinkBool) playerPrefabs.Add(gameManager.pickupPrefabs[10]);
-            if (OptionsManager.Instance.WarpBool) playerPrefabs.Add(gameManager.pickupPrefabs[11]);
-            if (OptionsManager.Instance.TetherBool) playerPrefabs.Add(gameManager.pickupPrefabs[12]);
-            if (OptionsManager.Instance.MudBool) playerPrefabs.Add(gameManager.pickupPrefabs[13]);
-            if (OptionsManager.Instance.GambitBool) playerPrefabs.Add(gameManager.pickupPrefabs[14]);
-            if (OptionsManager.Instance.GorbinoBool) playerPrefabs.Add(gameManager.pickupPrefabs[15]);
-        }
-        else if (OptionsManager.Instance == null)
-        {
-            playerPrefabs = gameManager.pickupPrefabs;
-        }
-    }
+    //private void Awake()
+    //{
+    //    GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    //    if (OptionsManager.Instance != null)
+    //    {
+    //        if (OptionsManager.Instance.FireballBool) playerPrefabs.Add(gameManager.pickupPrefabs[0]);
+    //        if (OptionsManager.Instance.IceBool) playerPrefabs.Add(gameManager.pickupPrefabs[1]);
+    //        if (OptionsManager.Instance.LightningBool) playerPrefabs.Add(gameManager.pickupPrefabs[2]);
+    //        if (OptionsManager.Instance.ShadowBool) playerPrefabs.Add(gameManager.pickupPrefabs[3]);
+    //        if (OptionsManager.Instance.GreenBool) playerPrefabs.Add(gameManager.pickupPrefabs[4]);
+    //        if (OptionsManager.Instance.StoneBool) playerPrefabs.Add(gameManager.pickupPrefabs[5]);
+    //        if (OptionsManager.Instance.ChronosBool) playerPrefabs.Add(gameManager.pickupPrefabs[6]);
+    //        if (OptionsManager.Instance.GeminiBool) playerPrefabs.Add(gameManager.pickupPrefabs[7]);
+    //        if (OptionsManager.Instance.PiscesBool) playerPrefabs.Add(gameManager.pickupPrefabs[8]);
+    //        if (OptionsManager.Instance.JollyBool) playerPrefabs.Add(gameManager.pickupPrefabs[9]);
+    //        if (OptionsManager.Instance.BlinkBool) playerPrefabs.Add(gameManager.pickupPrefabs[10]);
+    //        if (OptionsManager.Instance.WarpBool) playerPrefabs.Add(gameManager.pickupPrefabs[11]);
+    //        if (OptionsManager.Instance.TetherBool) playerPrefabs.Add(gameManager.pickupPrefabs[12]);
+    //        if (OptionsManager.Instance.MudBool) playerPrefabs.Add(gameManager.pickupPrefabs[13]);
+    //        if (OptionsManager.Instance.GambitBool) playerPrefabs.Add(gameManager.pickupPrefabs[14]);
+    //        if (OptionsManager.Instance.GorbinoBool) playerPrefabs.Add(gameManager.pickupPrefabs[15]);
+    //    }
+    //    else if (OptionsManager.Instance == null)
+    //    {
+    //        playerPrefabs = gameManager.pickupPrefabs;
+    //    }
+    //}
 }
